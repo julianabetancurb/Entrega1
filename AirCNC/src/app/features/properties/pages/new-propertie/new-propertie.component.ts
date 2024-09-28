74f0297a53017b977b50c74b9ec453e3b532d6bd
sim3075 <simonmiranda30dic@gmail.com>
2024-09-28 05:32:15 -0500

    add new features

---
+++ AirCNC/src/app/features/properties/pages/new-propertie/new-propertie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-propertie',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-propertie.component.html',
+  styleUrl: './new-propertie.component.css'
+})
+export class NewPropertieComponent {
+
+}
